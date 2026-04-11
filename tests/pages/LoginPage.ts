af45a0c6ddeca67f47ef64bdbf2153dbf648d4ae
Jhoser Juarez <jhoserjuarezm@gmail.com>
2026-04-10 23:13:18 -0500

    Se forzó a que se limpié caché siempre al acceder a shop

--- tests/pages/LoginPage.ts
+++ tests/pages/LoginPage.ts
@@ -1,35 +1,35 @@
-import { Page, Locator, expect } from '@playwright/test';
+import { Page, Locator, expect } from "@playwright/test";
 
 export class LoginPage {
   readonly page: Page;
 
   // 🎯 Locators
-  readonly btnShopHere:   Locator;
-  readonly btnPerfil1:   Locator;
+  readonly btnShopHere: Locator;
+  readonly btnPerfil1: Locator;
   readonly inputUsername: Locator;
   readonly inputPassword: Locator;
-  readonly btnLogin:      Locator;
-  readonly btnPerfil:     Locator;
-  
+  readonly btnLogin: Locator;
+  readonly btnPerfil: Locator;
 
   constructor(page: Page) {
     this.page = page;
 
-    this.btnShopHere   = page.getByRole('button', { name: 'Shop Here' });
-    this.btnPerfil1     = page.locator('i.mdi-account-circle').locator('..');
+    this.btnShopHere = page.getByRole("button", { name: "Shop Here" });
+    this.btnPerfil1 = page.locator("i.mdi-account-circle").locator("..");
     this.inputUsername = page.locator('[data-test="username-input"]');
     this.inputPassword = page.locator('[data-test="password-input"]');
-    this.btnLogin      = page.locator('[data-test="login-button"]');
-    this.btnPerfil     = page.locator('button.icon-login-user');        
-             
+    this.btnLogin = page.locator('[data-test="login-button"]');
+    this.btnPerfil = page.locator("button.icon-login-user");
   }
 
   getUsernameLabel(username: string): Locator {
-    return this.page.locator('.item-info', { hasText: username });
+    return this.page.locator(".item-info", { hasText: username });
   }
 
   async goto() {
-    await this.page.goto('/');
+    await this.page.context().clearCookies();
+    await this.page.context().clearPermissions();
+    await this.page.goto("/");
   }
 
   async login(username: string, password: string) {
@@ -42,7 +42,6 @@ export class LoginPage {
     await this.btnLogin.click();
   }
 
-  
   async verifyLoginSuccess(username: string) {
     await this.btnPerfil.click();
     await expect(this.getUsernameLabel(username)).toBeVisible();
